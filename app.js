b28dc3a752d00726de8f5e495676decd385256dd
Tharit Tothong <litchirhythm@gmail.com>
2013-04-27 00:17:23 -0400

    osc working!

--- app.js
+++ app.js
@@ -7,4 +7,10 @@ var client = new osc.Client('localhost', 4343);
 io.sockets.on('connection', function(socket) {
 	util.log('Say hello to' + socket.id);
 	client.send('/oscAddress', 'fucku uuuuuuu');
+
+	socket.on('xy', function(data) {
+		util.log(data.x + " " + data.y);
+		client.send('/xy/x', data.x);
+		client.send('/xy/y', data.y);
+	});
 });
